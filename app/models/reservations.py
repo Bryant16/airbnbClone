ed97ec248206f5fe3724629406a4ca4c0affed3b
Andrew Dotterer <adotterer@gmail.com>
2021-01-26 10:39:18 -0800

    search shows reservations as part of resposne

--- app/models/reservations.py
+++ app/models/reservations.py
@@ -7,5 +7,14 @@ class Reservation(db.Model):
   guest_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
   property_id = db.Column(db.Integer, db.ForeignKey("properties.id"), nullable=False)
   date = db.Column(db.DateTime, nullable=False)
-  property = db.relationship("Property")
+  listing = db.relationship("Property")
   user = db.relationship("User")
+
+  @property
+  def to_dict(self):
+    return {
+      "id": self.id,
+      "guest_id": self.guest_id,
+      "property_id": self.property_id,
+      "date": self.date
+    }
