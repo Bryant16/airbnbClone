19e170b9bb5db1f467a0a48f2a4637ce023a4ac4
Joshua <j.maxwell.alan@gmail.com>
2021-01-27 12:24:18 -0500

    add profile reducer to root reducer

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -1,27 +1,26 @@
-import { createStore, combineReducers, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import sessionReducer from "./session";
-import propertyPageReducer from "./propertyPage";
-import searchReducer from "./search";
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import sessionReducer from './session';
+import propertyPageReducer from './propertyPage';
+import searchReducer from './search';
+import profilePageReducer from './profilePage';
 
 const rootReducer = combineReducers({
   session: sessionReducer,
   property: propertyPageReducer,
   search: searchReducer,
+  profile: profilePageReducer
 });
 
 let enhancer;
 
-if (process.env.NODE_ENV === "production") {
-  enhancer = applyMiddleware(thunk);
-} else {
-  const logger = require("redux-logger").default;
-  const composeEnhancers =
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+if (process.env.NODE_ENV === 'production') enhancer = applyMiddleware(thunk);
+else {
+  const logger = require('redux-logger').default;
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
 }
 
-const configureStore = (preloadedState) =>
-  createStore(rootReducer, preloadedState, enhancer);
+const configureStore = (preloadedState) => createStore(rootReducer, preloadedState, enhancer);
 
 export default configureStore;
--- react-app/src/store/profilePage.js
+++ react-app/src/store/profilePage.js
@@ -5,6 +5,7 @@ const lookup = user => ({ type: ENUMERATE, user });
 export const LookUp = userId => async dispatch => {
   const lookupResponse = await window.fetch(`/api/users/${userId}`);
   const { user } = await lookupResponse.json();
+  return dispatch(lookup(user));
 };
 
 export default function profileReducer (state = { user: null }, action) {
