00bf8c09baa962133e9d86aec85f6cd4bcf54a7d
Andrew Dotterer <adotterer@gmail.com>
2021-01-27 15:55:39 -0800

    done with css for today

--- react-app/src/components/SearchResultListing.js
+++ react-app/src/components/SearchResultListing.js
@@ -3,13 +3,22 @@ import { NavLink } from "react-router-dom";
 
 const SearchResultListing = ({ listing }) => {
   return (
-      <NavLink to={`/properties/${listing.id}`}>
-        <div className='div__listings_property'>
-          <img src={listing.coverphoto_url} />
-          <p>{listing.city}</p>
-          <p>$ {listing.nightly_rate_usd}</p>
+    <NavLink to={`/properties/${listing.id}`}>
+      <div className="div__listings_property">
+        <div className="div__coverphoto">
+          <img
+            className="img__listing_coverphoto"
+            src={listing.coverphoto_url}
+          />
         </div>
-      </NavLink>
+        <div className="div__listings_info">
+          <p>{listing.city}</p>
+          <span className="span__nightly_rate">
+            $ {listing.nightly_rate_usd}
+          </span>
+        </div>
+      </div>
+    </NavLink>
   );
 };
 
