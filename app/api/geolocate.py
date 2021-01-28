b8b04b5e542f583a372e1fe65e759eb336cc4695
Joshua <j.maxwell.alan@gmail.com>
2021-01-27 23:27:53 -0500

    pausing for the evening

--- app/api/geolocate.py
+++ app/api/geolocate.py
@@ -1,7 +1,18 @@
-from geopy.geocoders import Nominatim
+from geopy.geocoders import Nominatim, GoogleV3
 geolocator = Nominatim(user_agent="CollegeBnB")
 
 def find_coordinates(location):
   geolocation = geolocator.geocode(location)
+  print('--------------------------')
+  print('--------------------------')
+  print('--------------------------')
+  print('--------------------------')
+  print('--------------------------')
+  print(geolocation[0])
+  print('--------------------------')
+  print('--------------------------')
+  print('--------------------------')
+  print('--------------------------')
+  print('--------------------------')
   response = {"latitude":geolocation.latitude, "longitude":geolocation.longitude}
   return response
