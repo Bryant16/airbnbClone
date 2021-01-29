79cbcd6828f3bd543340fa2b13710574756c2e87
Andrew Dotterer <adotterer@gmail.com>
2021-01-28 23:23:00 -0800

    nav links drop down menu

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,62 +1,72 @@
-import React, { useState, useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import LogoutButton from './auth/LogoutButton';
-import SearchBar from './SearchBar';
-import logo from '../collegeLogo.png';
-import './NavBar.css';
+import React, { useState, useEffect } from "react";
+import { NavLink, Redirect } from "react-router-dom";
+import { useSelector } from "react-redux";
+import LogoutButton from "./auth/LogoutButton";
+import SearchBar from "./SearchBar";
+import logo from "../collegeLogo.png";
+import { FaUserAlt } from "react-icons/fa";
+import { BsGrid3X3GapFill } from "react-icons/bs";
+import "./NavBar.css";
 
 const NavBar = () => {
   const user = useSelector((state) => state.session.user);
 
   return (
-    <div className='div__transparent_bg'>
-      <nav className='navbar'>
-        <div className='navbar_logo'>
-          <NavLink to='/' exact activeClassName='active'>
-            <img src={logo} alt='' />
+    <div className="div__transparent_bg">
+      <nav className="navbar">
+        <div className="navbar_logo">
+          <NavLink to="/" exact activeClassName="active">
+            <img src={logo} alt="" />
           </NavLink>
         </div>
-        <div className='navbar_search'>
-          <SearchBar />
+        <div className="navbar_search">
+          <SearchBar user={user} />
         </div>
-        <div className='navbar_links'>
-          <ul>
-            <li>
-              <NavLink to='/' exact activeClassName='active'>
+        <div className="div__user_button">
+          <div>
+            <FaUserAlt />
+          </div>
+          <div>
+            <BsGrid3X3GapFill />
+          </div>
+          <div className="navbar_links">
+            <div>
+              <NavLink to="/" exact activeClassName="active">
                 <button>Home</button>
               </NavLink>
-            </li>
-            <li>
-              {user
-                ? <LogoutButton />
-                : (
-                  <NavLink to='/login' exact activeClassName='active'>
-                    <button>Login</button>
-                  </NavLink>
-                  )}
-            </li>
-            <li>
-              <NavLink to='/sign-up' exact activeClassName='active'>
+            </div>
+            <div>
+              {user ? (
+                <>
+                  <LogoutButton />
+                </>
+              ) : (
+                <NavLink to="/login" exact activeClassName="active">
+                  <button>Login</button>
+                </NavLink>
+              )}
+            </div>
+            <div>
+              <NavLink to="/sign-up" exact activeClassName="active">
                 <button>Sign Up</button>
               </NavLink>
-            </li>
-            <li>
-              <NavLink to='/users' exact activeClassName='active'>
+            </div>
+            <div>
+              <NavLink to="/users" exact activeClassName="active">
                 <button>Users</button>
               </NavLink>
-            </li>
-            <li>
-              <NavLink to='/users/me'>
+            </div>
+            <div>
+              <NavLink to="/users/me">
                 <button>My Profile</button>
               </NavLink>
-            </li>
-            <li>
-              <NavLink to='/users/me/reservations'>
+            </div>
+            <div>
+              <NavLink to="/users/me/reservations">
                 <button>My Reservations</button>
               </NavLink>
-            </li>
-          </ul>
+            </div>
+          </div>
         </div>
       </nav>
     </div>
