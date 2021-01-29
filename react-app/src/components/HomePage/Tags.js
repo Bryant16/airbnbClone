dafe5982f56b6b4a543f0f3935868093993e89f0
bryant16 <kleinb1616@gmail.com>
2021-01-29 18:22:21 -0500

    all links updated

--- react-app/src/components/HomePage/Tags.js
+++ react-app/src/components/HomePage/Tags.js
@@ -7,10 +7,10 @@ const Tags = () => {
         <a href="https://github.com/adotterer">
           <i class="fab fa-github"></i>
         </a>
-        <a href="">
+        <a href="https://www.linkedin.com/in/andrew-dotterer-6338aa97/">
           <i class="fab fa-linkedin-in"></i>
         </a>
-        <a href="">
+        <a href="mailto:adotterer@gmail.com">
           <i class="far fa-envelope"></i>
         </a>
       </li>
@@ -21,7 +21,7 @@ const Tags = () => {
         <a href="https://www.linkedin.com/in/bryant-klein-927915134/">
           <i class="fab fa-linkedin-in"></i>
         </a>
-        <a href="">
+        <a href="mailto:kleinb1616@gmail.com">
           <i class="far fa-envelope"></i>
         </a>
       </li>
@@ -29,10 +29,10 @@ const Tags = () => {
         <a href="https://github.com/monaco021r">
           <i class="fab fa-github"></i>
         </a>
-        <a href="">
+        <a href="https://www.linkedin.com/mwlite/in/james-monaco-770127175">
           <i class="fab fa-linkedin-in"></i>
         </a>
-        <a href="">
+        <a href="mailto:monaco.james18@gmail.com">
           <i class="far fa-envelope"></i>
         </a>
       </li>
@@ -43,7 +43,7 @@ const Tags = () => {
         <a href="https://www.linkedin.com/in/joshua-summers-a761b1129/">
           <i class="fab fa-linkedin-in"></i>
         </a>
-        <a href="">
+        <a href="mailto:j.maxwell.alan@gmail.com">
           <i class="far fa-envelope"></i>
         </a>
       </li>
