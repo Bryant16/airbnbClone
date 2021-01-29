3384b6256c8da9dbcfba0b1cf49dd41619301fa9
james <monaco.james18@gmail.com>
2021-01-29 15:37:48 -0600

    100 properties added

--- app/seeds/properties.py
+++ app/seeds/properties.py
@@ -26,6 +26,28 @@ def seed_properties():
     ))
     db.session.commit()
 
+def seed_properties():
+  for i in range(42,144):
+    local = fake.local_latlng()
+    db.session.add(Property(
+      owner_id= 1,
+      coverphoto_id = i,
+      private = True,
+      nightly_rate_usd= 10.00 * i,
+      address1 = fake.street_address(),
+      address2 = f'{random.randrange(1,2000)} apt',
+      city= local[2],
+      zip_code= 00000,
+      latitude=local[0],
+      longitude=local[1],
+      listing_title =fake.sentence(nb_words=10, variable_nb_words=False),
+      description= fake.sentence(nb_words=15, variable_nb_words=False),
+      check_in= 1500,
+      check_out= 0000,
+      guest_spots=i % 20 + 1
+    ))
+    db.session.commit()
+
 
 def undo_properties():
   db.session.execute('TRUNCATE properties;')
