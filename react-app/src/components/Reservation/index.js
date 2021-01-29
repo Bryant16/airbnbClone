aed13c1700eba5e9d98c128460bee97c18556066
bryant16 <kleinb1616@gmail.com>
2021-01-28 22:09:40 -0500

    added price per night

--- react-app/src/components/Reservation/index.js
+++ react-app/src/components/Reservation/index.js
@@ -9,7 +9,8 @@ import { Redirect } from "react-router-dom";
 const Reservation = ({ property }) => {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
-  const [numGuest, setNumGuest] = useState(1)
+  const [numGuest, setNumGuest] = useState(1);
+  const numNights = ((Math.abs(endDate-startDate)) / (1000 * 60 * 60 * 24));
   const {user} =  useSelector((state) => state.session);
   const history = useHistory();
   const handleSelect= (ranges)=> {
@@ -56,10 +57,8 @@ const Reservation = ({ property }) => {
         }
           history.push('/users/me')
           // alert(`Your Reservation for ${numGuest}, on ${startDate} until ${endDate} was created`)
-          // console.log(newReservation)
       }
-      
-  }
+    }
   return (
     <div>
       <form>
@@ -67,6 +66,7 @@ const Reservation = ({ property }) => {
         <DateRange  ranges={[selectionRange]} onChange={handleSelect} />
         <label>Number of Guests</label>
         <input type='integer' onChange={(e)=> setNumGuest(e.target.value)}></input>
+        {numNights != 0 && <label>Price Per Night: ${numNights*property.nightly_rate_usd}</label>}
         <button onClick={handleSubmit}>Reserve Now</button>
       </form>
     </div>
