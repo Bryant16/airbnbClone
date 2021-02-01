a1365e613be2e4f38946138452fea47e8394b0b6
Joshua <j.maxwell.alan@gmail.com>
2021-02-01 00:35:20 -0500

    one last comment chunk

--- react-app/src/components/PropertyReviewPage/RadioSelector.js
+++ react-app/src/components/PropertyReviewPage/RadioSelector.js
@@ -2,22 +2,6 @@ import React, { useState } from 'react';
 import { nanoid } from 'nanoid';
 import { FaStar } from 'react-icons/fa';
 
-// export default function RadioSelector ({ label, count, checked, onChange }) {
-//   const inputs = [];
-//   for (let i = 0; i < count; i++) {
-//     inputs.push(<input type='radio' key={nanoid()} value={i + 1} checked={checked === `${i + 1}`} onChange={({ target: { value } }) => onChange(value)} />);
-//   }
-//   return (
-//     <div className='inputContainer'>
-//       <label>
-//         {label}
-//       </label>
-//       <div>
-//         {inputs}
-//       </div>
-//     </div>
-//   );
-// }
 export default function RadioSelector ({ label, count, checked, onChange }) {
   const [ratings, setRatings] = useState(null);
   const [hover, setHover] = useState(null);
