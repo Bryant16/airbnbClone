5061c623acfa76e12d505e40d18fa406b1429db3
Joshua <j.maxwell.alan@gmail.com>
2021-04-23 11:18:29 -0400

    create String.prototype.toTitleCase method

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -6,6 +6,15 @@ import './index.css';
 import App from './App';
 import createStore from './store';
 
+// eslint-disable-next-line no-extend-native
+String.prototype.toTitleCase = function () {
+  if (!this.match(/ /g)) return [this[0].toUpperCase(), this.slice(1)].join('');
+  return this
+    .split(' ')
+    .map($ => [$[0].toUpperCase(), $.slice(1)].join(''))
+    .join(' ');
+};
+
 const store = createStore();
 
 if (process.env.NODE_ENV !== 'production') {
