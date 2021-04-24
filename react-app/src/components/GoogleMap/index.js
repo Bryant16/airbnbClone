bca166317a00aec6943cc7b4d21838a784c2540d
Joshua <j.maxwell.alan@gmail.com>
2021-04-23 20:38:17 -0400

    remove unnecessary self-referrential useEffect

--- react-app/src/components/GoogleMap/index.js
+++ react-app/src/components/GoogleMap/index.js
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from "react";
-import GoogleMapReact from "google-map-react";
-import { nanoid } from "nanoid";
-import { NavHashLink } from "react-router-hash-link";
-import { useDispatch, useSelector } from "react-redux";
-import { focusListing } from "../../store/search";
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { nanoid } from 'nanoid';
+import { NavHashLink } from 'react-router-hash-link';
+import { useDispatch, useSelector } from 'react-redux';
+import { focusListing } from '../../store/search';
 
-import "./map.css";
+import './map.css';
 
 const Pin = ({ searchResult }) => {
   const dispatch = useDispatch();
   const focusId = useSelector((state) => state.search.focusId);
 
-  async function clickPinHandler(e) {
+  async function clickPinHandler (e) {
     e.preventDefault();
     dispatch(focusListing(searchResult.id));
   }
@@ -19,14 +19,14 @@ const Pin = ({ searchResult }) => {
   return (
     <NavHashLink smooth to={`#listing_${searchResult.id}`}>
       <div
-        className="pin"
+        className='pin'
         onClick={clickPinHandler}
         style={
           focusId === searchResult.id
             ? {
-                color: "#ff3a5c",
-                fontWeight: "700",
-                border: "1px solid #ff3a5c",
+                color: '#ff3a5c',
+                fontWeight: '700',
+                border: '1px solid #ff3a5c'
               }
             : null
         }
@@ -39,44 +39,19 @@ const Pin = ({ searchResult }) => {
 
 const GoogleMap = ({ locationObj, searchResults }) => {
   const focusId = useSelector((state) => state.search.focusId);
-  const searchLocation = useSelector((state) => state.search.searchLocation);
-  // const propsNearSchools = useSelector((state) => state.propsNearSchools);
-  const [focusZoom, setFocusZoom] = useState(locationObj);
-
-  useEffect(() => {
-    if (focusId > 0) {
-      const focusMap = searchResults
-        .filter((result) => {
-          return result.id === focusId;
-        })
-        .map((result) => {
-          return {
-            lat: result.latitude,
-            lng: result.longitude,
-          };
-        });
-      setFocusZoom(focusMap[0]);
-    } else {
-      setFocusZoom(locationObj);
-    }
-  }, [focusId, searchResults, locationObj]);
-
-  useEffect(() => {
-    setFocusZoom(searchLocation);
-  }, [searchLocation]);
 
   return (
-    <div className="map">
-      {!locationObj && "loading...."}
-      {locationObj && focusZoom && (
-        <div className="google-map">
+    <div className='map'>
+      {!locationObj && 'loading....'}
+      {locationObj && (
+        <div className='google-map'>
           <GoogleMapReact
             bootstrapURLKeys={{
-              key: process.env.REACT_APP_API_KEY,
+              key: process.env.REACT_APP_API_KEY
             }}
             // defaultCenter={{ lat: 40.7128, lng: -74.006 }}
-            center={focusZoom || { lat: 40.7128, lng: -74.006 }}
-            zoom={focusId > 0 ? 12 : 10}
+            center={locationObj}
+            zoom={focusId ? 12 : 10}
             defaultZoom={10}
           >
             {searchResults &&
