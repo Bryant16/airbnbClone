0e24613f923fa3def3aa1f87465883bdee2dd9f0
josh <j.maxwell.alan@gmail.com>
2021-04-24 23:06:27 -0400

    refactor login and signup forms and styling to fit modal

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
-import { Redirect } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { nanoid } from 'nanoid';
 
-import { SignUp } from '../../store/session';
+import { popLogin, teardown } from '../../store/modal';
+import { SignUp, LogIn } from '../../store/session';
 
 import './signupForm.css';
 
 const SignUpForm = () => {
   const dispatch = useDispatch();
-  const user = useSelector(state => state.session.user);
+  const after = useSelector(state => state.modal.after);
 
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
@@ -21,9 +21,17 @@ const SignUpForm = () => {
     e.preventDefault();
     if (password === repeatPassword) {
       dispatch(SignUp(username, email, password))
+        .then(() => {
+          dispatch(teardown());
+          after && after();
+        })
         .catch(err => setErrors(err.errors || []));
     } else setErrors(['Passwords must match.']);
   };
+
+  const demoLogin = () => dispatch(LogIn('demo@aa.io', 'password'));
+
+  const switchForm = () => dispatch(popLogin());
 
   const updateUsername = ({ target: { value } }) => {
     setUsername(value);
@@ -41,66 +49,82 @@ const SignUpForm = () => {
     setRepeatPassword(value);
   };
 
-  return user
-    ? <Redirect to='/' />
-    : (
-      <div className='div__container_form'>
-        <form className='form__container_div' onSubmit={onSignUp}>
-          <div>
-            {errors.map((error) => (
-              <div key={nanoid()}>
-                {error}
-              </div>
-            ))}
-          </div>
-          <div className='userName__container'>
-            <label>User Name:</label>
-            <input
-              type='text'
-              name='username'
-              onChange={updateUsername}
-              value={username}
-            />
-          </div>
-          <div className='email__container'>
-            <label>Email:</label>
-            <input
-              type='text'
-              name='email'
-              onChange={updateEmail}
-              value={email}
-            />
-          </div>
-          <div className='password_container'>
-            <label>Password:</label>
-            <input
-              type='password'
-              name='password'
-              onChange={updatePassword}
-              value={password}
-            />
-          </div>
-          <div className='password_container'>
-            <label>Repeat Password:</label>
-            <input
-              type='password'
-              name='repeat_password'
-              onChange={updateRepeatPassword}
-              value={repeatPassword}
-              required
-            />
-          </div>
-          <div className='signup__btn'>
-            <button
-              className='sub__button'
-              type='submit'
-            >
-              Sign Up
-            </button>
-          </div>
-        </form>
-      </div>
-      );
+  return (
+    <div className='div__container_form signup'>
+      <form className='form__container_div' onSubmit={onSignUp}>
+        {errors.length
+          ? (
+            <div>
+              {errors.map((error) => (
+                <div key={nanoid()}>
+                  {error}
+                </div>
+              ))}
+            </div>
+            )
+          : null}
+        <div className='userName__container'>
+          <input
+            type='text'
+            name='username'
+            placeholder='Username'
+            onChange={updateUsername}
+            value={username}
+          />
+        </div>
+        <div className='email__container'>
+          <input
+            type='text'
+            name='email'
+            placeholder='Email'
+            onChange={updateEmail}
+            value={email}
+          />
+        </div>
+        <div className='password_container'>
+          <input
+            type='password'
+            name='password'
+            placeholder='Password'
+            onChange={updatePassword}
+            value={password}
+          />
+        </div>
+        <div className='password_container'>
+          <input
+            type='password'
+            name='repeat_password'
+            placeholder='Confirm password'
+            onChange={updateRepeatPassword}
+            value={repeatPassword}
+            required
+          />
+        </div>
+        <div className='button-group'>
+          <button
+            className='grouped-button'
+            type='submit'
+          >
+            Sign Up
+          </button>
+          <button
+            type='button'
+            className='grouped-button'
+            onClick={demoLogin}
+          >
+            Demo Login
+          </button>
+        </div>
+        <button
+          className='grouped-button'
+          onClick={switchForm}
+          type='button'
+        >
+          Already have an account?
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default SignUpForm;
