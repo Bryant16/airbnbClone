8e0f91e5b8dffc918c3061da55db7e28348c50d2
josh <j.maxwell.alan@gmail.com>
2021-05-06 00:07:31 -0400

    prep styling and transitions for revamped property page

--- react-app/src/components/MapReel/index.js
+++ react-app/src/components/MapReel/index.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import GoogleMap from '../GoogleMap';
 import SearchResultListing from './SearchResultListing';
@@ -7,7 +7,12 @@ import { SetReelElement } from '../../store/mapReel';
 
 export default function MapReel ({ listings }) {
   const dispatch = useDispatch();
+
+  const reelMode = useSelector(state => state.mapReel.mode);
+
   const [showPrivate, setShowPrivate] = useState(false);
+  const [left, setLeft] = useState('0vw');
+  const [sliderWidth, setSliderWidth] = useState(40);
 
   const togglePrivate = () => setShowPrivate(p => !p);
 
@@ -17,32 +22,59 @@ export default function MapReel ({ listings }) {
     dispatch(SetReelElement(reelRef.current));
   }, [dispatch]);
 
+  useEffect(() => {
+    if (reelMode === 'listings') {
+      setLeft('0vw');
+      setSliderWidth(40);
+    }
+    if (reelMode === 'property') {
+      setLeft('-40vw');
+      setSliderWidth(70);
+    }
+  }, [reelMode]);
+
   return (
     <div className='listingMapContainer'>
       <div
-        ref={reelRef}
-        className='listingMapContainer_listings'
+        className='listingProperty slider'
+        style={{
+          left,
+          width: `${sliderWidth}vw`
+        }}
       >
-        {listings.length
-          ? (
-            <button
-              className='button__filter'
-              onClick={togglePrivate}
-              style={{
-                backgroundColor: showPrivate === true ? 'lightgrey' : 'white'
-              }}
-            >
-              Private
-            </button>
-            )
-          : <h1>Sorry, no listings in this area.</h1>}
-        {listings
-          .filter((result) => ((showPrivate === result.private) || !showPrivate))
-          .map((result, idx) => (
-            <SearchResultListing key={idx} listing={result} />
-          ))}
+        <div
+          ref={reelRef}
+          className='listingMapContainer_listings'
+        >
+          {listings.length
+            ? (
+              <button
+                className='button__filter'
+                onClick={togglePrivate}
+                style={{
+                  backgroundColor: showPrivate === true ? 'lightgrey' : 'white'
+                }}
+              >
+                Private
+              </button>
+              )
+            : <h1>Sorry, no listings in this area.</h1>}
+          {listings
+            .filter((result) => ((showPrivate === result.private) || !showPrivate))
+            .map((result, idx) => (
+              <SearchResultListing key={idx} listing={result} />
+            ))}
+        </div>
+        <div className='listingMapContainer_property'>
+          <h1>Placeholder</h1>
+        </div>
       </div>
-      <div className='listingMapContainer__googlemap'>
+      <div
+        className='listingMapContainer__googlemap'
+        style={{
+          width: `${100 - sliderWidth}vw`
+        }}
+      >
         <div className='button map-expand' />
         <GoogleMap
           searchResults={listings}
