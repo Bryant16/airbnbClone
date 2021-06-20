002a95f6b43a86734df5df49dc7995e05a7898b2
Joshua <j.maxwell.alan@gmail.com>
2021-06-19 23:34:34 -0400

    standardize shape of auth routes

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -8,76 +8,75 @@ auth_routes = Blueprint('auth', __name__)
 
 
 def validation_errors_to_error_messages(validation_errors):
-    """
-    Simple function that turns the WTForms validation errors into a simple list
-    """
-    errorMessages = []
-    for field in validation_errors:
-        for error in validation_errors[field]:
-            errorMessages.append(f"{field} : {error}")
-    return errorMessages
+  """
+  Simple function that turns the WTForms validation errors into a simple list
+  """
+  errorMessages = []
+  for field in validation_errors:
+    for error in validation_errors[field]:
+      errorMessages.append(error)
+  return errorMessages
 
 
 @auth_routes.route('/')
 def authenticate():
-    """
-    Authenticates a user.
-    """
-    if current_user.is_authenticated:
-        return {'user': current_user.to_dict()}
-    return {'user':{'errors': ['Unauthorized']}}
+  """
+  Authenticates a user.
+  """
+  if current_user.is_authenticated:
+    return {'user': current_user.to_dict()}
+  return {'errors': ['Unauthorized']}
 
 
 @auth_routes.route('/login', methods=['POST'])
 def login():
-    """
-    Logs a user in
-    """
-    form = LoginForm()
-    print(request.get_json())
-    # Get the csrf_token from the request cookie and put it into the
-    # form manually to validate_on_submit can be used
-    form['csrf_token'].data = request.cookies['csrf_token']
-    if form.validate_on_submit():
-        # Add the user to the session, we are logged in!
-        user = User.query.filter(User.email == form.data['email']).first()
-        login_user(user)
-        return {"user": user.to_dict()}
-    return {'user': {'errors': validation_errors_to_error_messages(form.errors)}}, 401
+  """
+  Logs a user in
+  """
+  form = LoginForm()
+  # Get the csrf_token from the request cookie and put it into the
+  # form manually to validate_on_submit can be used
+  form['csrf_token'].data = request.cookies['csrf_token']
+  if form.validate_on_submit():
+    # Add the user to the session, we are logged in!
+    user = User.query.filter(User.email == form.data['email']).first()
+    login_user(user)
+    return {"user": user.to_dict()}
+  return {'errors': validation_errors_to_error_messages(form.errors)}, 401
 
 
 @auth_routes.route('/logout')
 def logout():
-    """
-    Logs a user out
-    """
-    logout_user()
-    return {'message': 'User logged out'}
+  """
+  Logs a user out
+  """
+  logout_user()
+  return 200
 
 
 @auth_routes.route('/signup', methods=['POST'])
 def sign_up():
-    """
-    Creates a new user and logs them in
-    """
-    form = SignUpForm()
-    form['csrf_token'].data = request.cookies['csrf_token']
-    if form.validate_on_submit():
-        user = User(
-            username=form.data['username'],
-            email=form.data['email'],
-            password=form.data['password']
-        )
-        db.session.add(user)
-        db.session.commit()
-        login_user(user)
-        return jsonify({'user': user.to_dict()})
-    return {'user': {'errors': validation_errors_to_error_messages(form.errors)}}, 401
+  """
+  Creates a new user and logs them in
+  """
+  form = SignUpForm()
+  form['csrf_token'].data = request.cookies['csrf_token']
+  if form.validate_on_submit():
+    user = User(
+      username=form.data['username'],
+      email=form.data['email'],
+      password=form.data['password']
+    )
+    db.session.add(user)
+    db.session.commit()
+    login_user(user)
+    return {'user': user.to_dict()}
+  return {'errors': validation_errors_to_error_messages(form.errors)}, 401
 
 
 @auth_routes.route('/unauthorized')
 def unauthorized():
-    """
-    Returns unauthorized JSON when flask-login authentication fails
-    """
-    return {'user':{'errors': ['Unauthorized']}}, 401
+  """
+  Returns unauthorized JSON when flask-login authentication fails
+  """
+  return {'errors': ['Unauthorized']}, 401
