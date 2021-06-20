67bcf63ba787da1e6eb695aaf2dc4363fd6ff398
Joshua <j.maxwell.alan@gmail.com>
2021-06-19 23:46:26 -0400

    make use of implicit return

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -24,19 +24,17 @@ export default function SchoolListings () {
 
         <div className='div__school_container'>
           <div className='school_button_container_1'>
-            {schools.slice(0, 5).map((school, idx) => {
-              return (
-                <div className='school_buttons' key={idx}>
-                  <Link to={`/school/${school.id}/${school.name}`}>
-                    <img
-                      className='schoolLogoImage'
-                      src={school.logo_url}
-                      alt='schoolImage'
-                    />
-                  </Link>
-                </div>
-              );
-            })}
+            {schools.slice(0, 5).map((school, idx) => (
+              <div className='school_buttons' key={idx}>
+                <Link to={`/school/${school.id}/${school.name}`}>
+                  <img
+                    className='schoolLogoImage'
+                    src={school.logo_url}
+                    alt='schoolImage'
+                  />
+                </Link>
+              </div>
+            ))}
           </div>
           <div className='school_button_container_2'>
             {schools.slice(5, 10).map((school, idx) => (
