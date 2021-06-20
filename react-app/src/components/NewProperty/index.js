2502715349e89c27582864dd789a528034759a38
Joshua <j.maxwell.alan@gmail.com>
2021-06-20 01:04:29 -0400

    move property creation into propertyPage reducer

--- react-app/src/components/NewProperty/index.js
+++ react-app/src/components/NewProperty/index.js
@@ -1,10 +1,17 @@
 import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+import { SetCurrentErrors } from '../../store/errors';
+import { CreateProperty } from '../../store/propertyPage';
 
 import './index.css';
 
 export default function NewProperty () {
+  const dispatch = useDispatch();
+
   const history = useHistory();
+
   const [listingTitle, updateTitle] = useState('');
   const [address1, updateAddr1] = useState('');
   const [address2, updateAddr2] = useState('');
@@ -12,51 +19,32 @@ export default function NewProperty () {
   const [zipCode, updateZipCode] = useState('');
   const [isPrivate, setIsPrivate] = useState(false);
   const [description, updateDescription] = useState('');
-  const [checkIn, setCheckIn] = useState('12:00');
-  const [checkOut, setCheckOut] = useState('12:00');
+  const [checkIn, setCheckIn] = useState(1200);
+  const [checkOut, setCheckOut] = useState(1200);
   const [guestSpots, updateMaxOccupancy] = useState(2);
   const [nightlyRate, setRate] = useState(0);
-  const [pageErrors, setPageErrors] = useState([]);
 
-  const submit = async (e) => {
+  const submit = (e) => {
     e.preventDefault();
-    setPageErrors([]);
+    dispatch(SetCurrentErrors([]));
     const newProperty = {
       listingTitle,
-      private: isPrivate,
       nightlyRate,
       address1,
       address2,
       city,
       zipCode,
       description,
-      checkIn: +checkIn.replace(/:/g, ''),
-      checkOut: +checkOut.replace(/:/g, ''),
-      guestSpots
+      checkIn,
+      checkOut,
+      guestSpots,
+      private: isPrivate
     };
-    const response = await window.fetch('/api/property/', {
-      headers: { 'Content-type': 'application/json' },
-      method: 'POST',
-      body: JSON.stringify(newProperty)
-    });
-    const { success, id } = await response.json();
-    if (success && id) history.push(`/properties/${id}`);
-    else {
-      setPageErrors(["Sorry, our system culdn't seem to find that address."]);
-    }
+    dispatch(CreateProperty(newProperty, history));
   };
 
   return (
     <div className='new-property-page-container'>
-      {pageErrors.length
-        ? (
-          <div className='page-errors'>
-            <ul>
-              {pageErrors.map((err, idx) => <li key={idx}>{err}</li>)}
-            </ul>
-          </div>
-          )
-        : null}
       <form
         className='new-property-form'
         onSubmit={submit}
@@ -67,14 +55,12 @@ export default function NewProperty () {
             placeholder='Listing Title'
             value={listingTitle}
             onChange={({ target: { value } }) => updateTitle(value)}
-            required
           />
           <input
             type='text'
             placeholder='Address Line 1'
             value={address1}
             onChange={({ target: { value } }) => updateAddr1(value)}
-            required
           />
           <input
             type='text'
@@ -87,7 +73,6 @@ export default function NewProperty () {
             placeholder='City'
             value={city}
             onChange={({ target: { value } }) => updateCity(value)}
-            required
           />
           <input
             type='text'
@@ -95,7 +80,6 @@ export default function NewProperty () {
             value={zipCode}
             onChange={({ target: { value } }) => updateZipCode(value)}
             pattern='^\d{5}'
-            required
           />
         </div>
         <div className='checkin-checkout-privacy-occupancy'>
@@ -106,8 +90,8 @@ export default function NewProperty () {
               </label>
               <input
                 type='time'
-                value={checkIn}
-                onChange={({ target: { value } }) => setCheckIn(value)}
+                value={`${checkIn.toString().slice(0, 2)}:${checkIn.toString().slice(2)}`}
+                onChange={({ target: { value } }) => setCheckIn(+value.replace(/:/g, ''))}
               />
             </div>
             <div className='new-checkout'>
@@ -116,8 +100,8 @@ export default function NewProperty () {
               </label>
               <input
                 type='time'
-                value={checkOut}
-                onChange={({ target: { value } }) => setCheckOut(value)}
+                value={`${checkOut.toString().slice(0, 2)}:${checkOut.toString().slice(2)}`}
+                onChange={({ target: { value } }) => setCheckOut(+value.replace(/:/g, ''))}
               />
             </div>
           </div>
@@ -170,7 +154,6 @@ export default function NewProperty () {
           className='description'
           value={description}
           onChange={({ target: { value } }) => updateDescription(value)}
-          required
         />
         <button className='new-property-submit'>
           Make some money!
