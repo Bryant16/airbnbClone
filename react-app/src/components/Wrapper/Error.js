cef0e8c4d4e06f3611f3cc5cca774151c5dcc078
Joshua <j.maxwell.alan@gmail.com>
2021-06-19 23:13:45 -0400

    more consistent error styling

--- react-app/src/components/Wrapper/Error.js
+++ react-app/src/components/Wrapper/Error.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { SetCurrentErrors } from '../../store/errors';
@@ -6,17 +7,31 @@ export default function Error () {
   const dispatch = useDispatch();
 
   const currentErrors = useSelector(state => state.errors.current);
+  const { length } = currentErrors;
+
+  const [grow, setGrow] = useState(false);
 
   const onClose = () => dispatch(SetCurrentErrors([]));
+
+  useEffect(() => {
+    setGrow(!!length);
+  }, [length]);
+
+  console.log(length, grow);
+
   return (
     <div
-      className={`error-bar${
-        currentErrors.length ? ' grow' : ' shrink'
-      }`}
+      className='error-bar'
+      style={{
+        height: length * 50
+      }}
     >
       <div className='errors-body'>
         {currentErrors.map((err, idx) => (
-          <div key={idx} className='error-content'>
+          <div
+            key={idx}
+            className='error-content'
+          >
             {err}
           </div>
         ))}
