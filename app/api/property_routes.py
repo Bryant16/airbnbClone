73826206c2ba82d4963e01561f35a9770a4193fa
Joshua <j.maxwell.alan@gmail.com>
2021-06-20 01:04:05 -0400

    add verbose error handling in new property creation

--- app/api/property_routes.py
+++ app/api/property_routes.py
@@ -15,30 +15,48 @@ def soloProperty(id):
 @property_routes.route('/', methods=['POST'])
 def create_property():
   payload = request.get_json()
+  errors = []
+  if not payload['address1']:
+    errors.append('Please provide a primary street address')
+  if not payload['city']:
+    errors.append('Please provide a city')
+  if not payload['zipCode']:
+    errors.append('Please provide a zip code')
+  if not payload['listingTitle']:
+    errors.append('Please provide a title')
+  if not payload['description']:
+    errors.append('Please provide a description')
+  if not payload['checkIn']:
+    errors.append('Please provide a check-in time')
+  if not payload['checkOut']:
+    errors.append('Please provide a check-out time')
+  if not payload['guestSpots']:
+    errors.append('Please provide a maximum number of guests')
+  if len(errors):
+    return {'errors': errors}
   prep = f"{payload['address1']} {payload['address2']} {payload['city']} {payload['zipCode']}"
   coords = find_coordinates(prep)
-  try:
-    new_property = Property(
-      owner_id=current_user.id,
-      private=payload['private'],
-      nightly_rate_usd=payload['nightlyRate'],
-      address1=payload['address1'],
-      address2=payload['address2'],
-      city=payload['city'],
-      zip_code=payload['zipCode'],
-      latitude=coords['latitude'],
-      longitude=coords['longitude'],
-      listing_title=payload['listingTitle'],
-      description=payload['description'],
-      check_in=payload['checkIn'],
-      check_out=payload['checkOut'],
-      guest_spots=payload['guestSpots']
-    )
-    db.session.add(new_property)
-    db.session.commit()
-    return jsonify({'success': True, 'id': new_property.id})
-  except:
-    return jsonify({'success': False})
+  if not coords or not coords['latitude'] or not coords['longitude']:
+    return {'errors': ['Sorry, our system wasn\'t able to locate that address ']}
+  new_property = Property(
+    owner_id=current_user.id,
+    private=payload['private'],
+    nightly_rate_usd=payload['nightlyRate'],
+    address1=payload['address1'],
+    address2=payload['address2'],
+    city=payload['city'],
+    zip_code=payload['zipCode'],
+    latitude=coords['latitude'],
+    longitude=coords['longitude'],
+    listing_title=payload['listingTitle'],
+    description=payload['description'],
+    check_in=payload['checkIn'],
+    check_out=payload['checkOut'],
+    guest_spots=payload['guestSpots']
+  )
+  db.session.add(new_property)
+  db.session.commit()
+  return {'success': True, 'id': new_property.id}
 
 @property_routes.route('/<int:property_id>/booked/')
 @login_required
